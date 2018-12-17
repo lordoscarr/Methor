984aadc6e97ecb45fce2333fb2a658be630f560b
Oscar Andersson <paroscarandersson@live.com>
2018-12-17 17:29:05 +0100

    Rock Paper Scissors template

--- app/src/main/java/methor/se/methor/Minigames/RPSFragment.java
+++ app/src/main/java/methor/se/methor/Minigames/RPSFragment.java
@@ -1,32 +1,35 @@
 package methor.se.methor.Minigames;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
+import methor.se.methor.Models.RPS;
 import methor.se.methor.R;
 
-public class RPSFragment extends Fragment {
+public class RPSFragment extends Fragment implements SensorEventListener {
 
-    private enum Result{
-        WIN, DRAW, LOSS
-    }
-
-    private enum Choice {
-        ROCK(1, "ROCK"), PAPER(2, "PAPER"), SCISSORS(3, "SCISSORS");
-
-        private int type;
-        private String name;
-        Choice(int type, String name){ this.type = type; this.name = name; }
-        public int getType(){ return type; }
-        public String getName(){ return name; }
-
-    }
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private Spinner myChoiceSpinner;
+    private LinearLayout choiceLayout;
+    private TextView tvInstructions;
+    private TextView tvResult;
+    private TextView tvResultAi;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,55 +37,97 @@ public class RPSFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_rps, container, false);
         initializeComponents(view);
+        initializeSensors();
         return view;
     }
 
     private void initializeComponents(View view) {
         //Get UI elements
-        for (int i = 1; i <= 10; i++){
-            Choice myChoice = Choice.ROCK;
-            Choice opponentChoice = getOpponentChoice();
+        myChoiceSpinner = view.findViewById(R.id.myChoiceSpinner);
+        choiceLayout = view.findViewById(R.id.choiceLayout);
+        tvInstructions = view.findViewById(R.id.tvInstructions);
+        tvResult = view.findViewById(R.id.tvResult);
+        tvResultAi = view.findViewById(R.id.tvResultAi);
 
+        myChoiceSpinner.setAdapter(new ArrayAdapter<RPS.Choice>(getActivity(), R.layout.support_simple_spinner_dropdown_item, RPS.Choice.values()));
+        choiceLayout.setVisibility(View.GONE);
+    }
 
-            Log.d("Game " + i, "My choice: " + myChoice.getName() +
-                    ", Opponents choice: " + opponentChoice.getName() + ", Result: " + getResult(myChoice, opponentChoice));
+    private void initializeSensors() {
+        sensorManager = (SensorManager) getActivity().getSystemService(getContext().SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void onShake(){
+        if(shakeCount < 3){
+            tvInstructions.setText("Shake " + (3 - shakeCount) + " more times");
+        }else {
+            RPS.Choice myChoice = (RPS.Choice) myChoiceSpinner.getSelectedItem();
+            RPS.Result result = RPS.simulateGame(myChoice);
+            Log.d(this.getTag(), result.name());
         }
     }
 
-    //Rock, paper, scissors. Best of three.
-    private Choice getOpponentChoice(){
-        Random rand = new Random();
-        int choice = rand.nextInt(3);
-        return Choice.values()[choice];
+    @Override
+    public void onResume() {
+        super.onResume();
+        initializeSensors();
     }
 
-    private Result getResult(Choice myChoice, Choice opponentChoice){
-            switch (myChoice) {
-                case ROCK:
-                    if(opponentChoice == Choice.ROCK)
-                        return Result.DRAW;
-                    else if(opponentChoice == Choice.PAPER)
-                        return Result.LOSS;
-                    else if(opponentChoice == Choice.SCISSORS)
-                        return Result.WIN;
-                    break;
-                case PAPER:
-                    if(opponentChoice == Choice.ROCK)
-                        return Result.WIN;
-                    else if(opponentChoice == Choice.PAPER)
-                        return Result.DRAW;
-                    else if(opponentChoice == Choice.SCISSORS)
-                        return Result.LOSS;
-                    break;
-                case SCISSORS:
-                    if(opponentChoice == Choice.ROCK)
-                        return Result.LOSS;
-                    else if(opponentChoice == Choice.PAPER)
-                        return Result.WIN;
-                    else if(opponentChoice == Choice.SCISSORS)
-                        return Result.DRAW;
-                    break;
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
+    private static final int SHAKE_SLOP_TIME_MS = 500;
+    private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
+
+    private long shakeTimestamp;
+    private int shakeCount;
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+        float gX = x / SensorManager.GRAVITY_EARTH;
+        float gY = y / SensorManager.GRAVITY_EARTH;
+        float gZ = z / SensorManager.GRAVITY_EARTH;
+
+        // gForce will be close to 1 when there is no movement.
+        float gForce = (float) Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+        if (shakeCount >= 3) {
+            shakeCount = 0;
+
         }
-        return null;
+
+        if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+            final long now = System.currentTimeMillis();
+            // ignore shake events too close to each other (500ms)
+            if (shakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                return;
+            }
+
+            // reset the shake count after 3 seconds of no shakes
+            if (shakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                shakeCount = 0;
+            }
+
+            shakeTimestamp = now;
+            shakeCount++;
+            Log.d(this.getTag(), "onSensorChanged: Shake " + shakeCount);
+
+            onShake();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        Log.d(this.getTag(), "ACCURACY CHANGED " + i);
     }
 }

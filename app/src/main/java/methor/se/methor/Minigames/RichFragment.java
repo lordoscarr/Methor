b701d3e5490c33f3feff6ccfc4343485ae821ea5
RichardPahmp <pahmp.richard@gmail.com>
2019-01-06 19:03:23 +0100

    cleaning up

--- app/src/main/java/methor/se/methor/Minigames/RichFragment.java
+++ app/src/main/java/methor/se/methor/Minigames/RichFragment.java
@@ -4,21 +4,30 @@ package methor.se.methor.Minigames;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import methor.se.methor.R;
 
-public class RichFragment extends Fragment {
+public class RichFragment extends Fragment implements SensorEventListener{
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private TextView textView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_rich, container, false);
         initializeComponents(view);
@@ -27,6 +36,34 @@ public class RichFragment extends Fragment {
     }
 
     private void initializeComponents(View view){
+        sensorManager = (SensorManager) getActivity().getSystemService(getContext().SENSOR_SERVICE);
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+
+        textView = getActivity().findViewById(R.id.textView);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.i("asd", "0: " + event.values[0] + " 1: " + event.values[1] + " 2: " + event.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
